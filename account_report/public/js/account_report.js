f55b429269c7cbab9a88b7d9cc13a06a0e89aaf7
Dinesh Panchal <dineshpanchal432@gmail.com>
2018-09-28 14:45:22 +0530

    Update account_report.js

--- account_report/public/js/account_report.js
+++ account_report/public/js/account_report.js
@@ -5,7 +5,7 @@ $(function() {
 
 	$('.footer-powered').hide();  // or .remove();
 
-	$('.navbar-home').html('<img class="erpnext-icon" src="https://www.scantechlaser.com/images/favicon.ico"/>');
+	$('.navbar-home').html('<img class="erpnext-icon" src="https://www.digiwinx.com/images/logo.png"/>');
 
 });
 
